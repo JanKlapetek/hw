b4ceb39928d7c8c8f97800449e2708c773491431
JanKlapetek <jan.klapetek@gmail.com>
2024-04-30 10:25:00 +0200

    second commit

--- hw_10_part1_01.py
+++ hw_10_part1_01.py
@@ -10,3 +10,18 @@ class Seznam_cisel:
         else:
             print(f'Číslo {cislo} již v seznamu existuje.')
 
+    def odstranit_cislo(self, cislo):
+        if cislo in self.seznam:
+            self.seznam.remove(cislo)
+            print(f'Číslo {cislo} bylo odstraněno ze seznamu.')
+        else:
+            print(f'Číslo {cislo} není v seznamu.')
+
+    def zobrazit_obsah(self, sestupne=False):
+        if self.seznam:
+            print('Obsah seznamu:')
+            sorted_seznam = sorted(self.seznam, reverse=sestupne)
+            for cislo in sorted_seznam:
+                print(cislo)
+        else:
+            print('Seznam je prázdný.')
