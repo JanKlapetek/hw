4faca562faced00ab788aecf4ec5314806c2cd4e
JanKlapetek <jan.klapetek@gmail.com>
2024-05-02 10:42:22 +0200

    add class of Dict and add city

--- hw_11_part1_01.py
+++ hw_11_part1_01.py
@@ -2,13 +2,32 @@
 # Implementujte následující funkce: přidávání dat, mazání dat, vyhledávání dat, úpravy dat, ukládání a načítání dat (pomocí zabalení a rozbalení).
 
 import json
-capitals = {
-    'Italy': 'Rome',
-    'France': 'Paris',
-    'Czech': 'Prague'
-}
-serialized = json.dumps(capitals)
-print(serialized)
 
-deserialized = json.loads(serialized)
-print(deserialized)
+class Capitals:
+    def __init__(self):
+        self.slovnik = {}
+
+    def pridej_mesto(self, stat, mesto):
+        """
+        Přidá město do slovníku pod daný stát.
+        """
+        if stat in self.slovnik:
+            self.slovnik[stat].append(mesto)
+        else:
+            self.slovnik[stat] = [mesto]
+
+    def vypis_slovnik(self):
+        """
+        Vypíše obsah slovníku.
+        """
+        for stat, mesta in self.slovnik.items():
+            print(stat, mesta)
+
+# Příklad použití
+slovnik_mest = Capitals()
+slovnik_mest.pridej_mesto('Czech', 'Prague')
+slovnik_mest.pridej_mesto('France', 'Paris')
+slovnik_mest.pridej_mesto('Germany', 'Berlin')
+slovnik_mest.pridej_mesto('Italy', 'Rome')
+
+slovnik_mest.vypis_slovnik()
