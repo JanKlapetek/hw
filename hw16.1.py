ca5987ed3f8af171a839c157e4af5a4de11ccc05
Jan_win <jan.klapetek@gmail.com>
2024-06-02 18:59:58 +0200

    Threading in random numbers add, sum numbers and average.

---
+++ hw16.1.py
@@ -0,0 +1,44 @@
+import random
+import threading
+
+class ZpracovaniSeznamuCisel:
+    def __init__(self, pocet_prvku):
+        self.pocet_prvku = pocet_prvku
+        self.seznam_cisel = []
+        self.soucet_vysledek = None
+        self.prumer_vysledek = None
+
+    def napln_seznam(self):
+        self.seznam_cisel = [random.randrange(0,1000) for _ in range(self.pocet_prvku)]
+
+    def spocitej_soucet(self):
+        self.soucet_vysledek = sum(self.seznam_cisel)
+
+    def spocitej_prumer(self):
+        self.prumer_vysledek = sum(self.seznam_cisel) / self.pocet_prvku
+
+    def spust_vlakna(self):
+        vlakno_naplneni = threading.Thread(target=self.napln_seznam)
+        vlakno_soucet = threading.Thread(target=self.spocitej_soucet)
+        vlakno_prumer = threading.Thread(target=self.spocitej_prumer)
+
+        vlakno_naplneni.start()
+        vlakno_naplneni.join()
+
+        vlakno_soucet.start()
+        vlakno_prumer.start()
+
+        vlakno_soucet.join()
+        vlakno_prumer.join()
+
+    def zobraz_vysledky(self):
+        print(f'Vygenerovaný seznam {self.pocet_prvku} náhodných čísel: {self.seznam_cisel}')
+        print(f'Součet prvků seznamu: {self.soucet_vysledek}')
+        print(f'Průměr prvků seznamu: {self.prumer_vysledek:.3f}')
+
+
+if __name__ == '__main__':
+    pocet_prvku = 50
+    zpracovani = ZpracovaniSeznamuCisel(pocet_prvku)
+    zpracovani.spust_vlakna()
+    zpracovani.zobraz_vysledky()
